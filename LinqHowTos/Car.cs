014de225d6230b67f353ecbb8fdf199d36fdc8a7
btajahmadi <btajahmadi@gmail.com>
2018-09-18 15:51:29 +0430

    Car edited

--- LinqHowTos/Car.cs
+++ LinqHowTos/Car.cs
@@ -8,36 +8,55 @@ namespace LinqHowTos
 {
     public class Car
     {
-        public int Id { get; set; }
+        private int _id;
+        public int Id
+        {
+            get
+            {
+                Console.WriteLine("Id getter");
+                return _id;
+            }
+            set
+            {
+                Console.WriteLine("Id setter");
+                _id =  value;
+            }
+        }
         public string Manufacturer { get; set; }
         public string Model {get; set;}
         public string Year { get; set; }
         public int Cylinders { get; set; }
-        public static IEnumerable<Car> ProcessCars()
+        #region Parsing from CSV file
+        private static Car ParseCsvLine(string line)
         {
-            var vehicles = File.ReadAllLines("vehicles.csv")
-                                .Where(line => line.Length > 1).Skip(1);
-            
-            foreach (var vehicle in vehicles)
-            {
-                var columns = vehicle.Split(',');
-                yield return new Car() {
+                var columns = line.Split(',');
+                return new Car() {
                     Id = int.Parse(columns[43]),
                     Manufacturer = columns[46],
                     Model = columns[47],
                     Year = columns[63],
-                    Cylinders = ProcessCylinders(columns[22])
+                    Cylinders = ParseCylinders(columns[22])
                     };
-            }
         }
-
-        private static int ProcessCylinders(string c)
+        private static int ParseCylinders(string c)
         {
             int cylinders;
             int.TryParse(c, out cylinders);
             return cylinders;
         }
+        public static IEnumerable<Car> GetCarsFromCsv(string path)
+        {
+            return File.ReadAllLines(path)
+                       .Where(line => line.Length > 1)
+                       .Skip(1)
+                       .Select(Car.ParseCsvLine)
+                       .ToList();
+        }
+        #endregion Parsing from CSV file
     }
+
+
+
 
 
 
